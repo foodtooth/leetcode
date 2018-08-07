9d124fc7c9d074bc140d7a6f086477ae812168c6
d00441522 <d00441522@huawei.com>
2018-08-07 19:50:16 +0800

    Init

---
+++ python/two_sum/two_sum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1, length):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+    def twoSum2(self, nums, target):
+        diff_dict = {}
+        for i, num in enumerate(nums):
+            if num in diff_dict.keys():
+                return [diff_dict[num], i]
+            else:
+                diff_dict[target - num] = i
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
