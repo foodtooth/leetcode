07854608839f4324192712ab327b8d6fe15715b4
Michael Donux <michaeldx2008@gmail.com>
2019-01-19 03:59:55 -0500

    No. 4 median_of_two_sorted_arrays cpp solution

---
+++ 4_median_of_two_sorted_arrays/cpp/median_of_two_sorted_arrays.cpp
@@ -0,0 +1,39 @@
+/*
+ * median_of_two_sorted_arrays.cpp
+ *
+ *  Created on: Jan 18, 2019
+ *      Author: dx
+ */
+
+#include <vector>
+
+class Solution {
+ public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    std::vector<int> nums { };
+    int len1 = nums1.size();
+    int len2 = nums2.size();
+    int i { };
+    int j { };
+
+    while (i < len1 && j < len2) {
+      if (nums1[i] < nums2[j]) {
+        nums.push_back(nums1[i]);
+        ++i;
+      } else {
+        nums.push_back(nums2[j]);
+        ++j;
+      }
+    }
+    if (i < len1)
+      nums.insert(nums.end(), nums1.begin() + i, nums1.end());
+    if (j < len2)
+      nums.insert(nums.end(), nums2.begin() + j, nums2.end());
+
+    int len = nums.size();
+    if (len % 2 == 0)
+      return (nums[len / 2 - 1] + nums[len / 2]) / 2.0;
+    else
+      return nums[(len + 1) / 2 - 1] * 1.0;
+  }
+};
