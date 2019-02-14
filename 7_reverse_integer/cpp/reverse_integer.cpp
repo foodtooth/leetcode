8fbb28c2c781343b88dbe1841d93ebaf64a4e04e
Michael Donux <michaeldx2008@gmail.com>
2019-02-13 21:33:02 -0500

    No. 7 reverse_integer cpp solution

---
+++ 7_reverse_integer/cpp/reverse_integer.cpp
@@ -0,0 +1,29 @@
+/*
+ * reverse_integer.cpp
+ *
+ *  Created on: Jan 30, 2019
+ *      Author: dx
+ */
+
+#include <cstdint>
+#include <cinttypes>
+
+class Solution {
+ public:
+  int reverse(int x) {
+    std::int32_t result { 0 };
+    int pop { };
+    while (x != 0) {
+      if (result > 0 && result > INT32_MAX / 10) {
+        return 0;
+      }
+      if (result < 0 && result < INT32_MIN / 10) {
+        return 0;
+      }
+      pop = x % 10;
+      result = result * 10 + pop;
+      x /= 10;
+    }
+    return result;
+  }
+};
